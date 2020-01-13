c0228486f4bad6f89ae6552e2d366ca6a0ecad4f
Jacob LeCheminant <jacoblecheminant@Jacobs-MacBook-Pro.local>
2020-01-13 10:23:15 -0700

    Initial Commit

---
+++ Settings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Jacob LeCheminant on 1/13/20.
+//  Copyright © 2020 Jacob LeCheminant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
